b7b087c699dddd25f4b20398d4e6f34d24cf0101
aslamfam <81439199+aslamfam@users.noreply.github.com>
2021-04-14 16:52:22 +0530

    Update buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -5,7 +5,10 @@ $version=$BUILD_ID
 
 docker build -t apprepo:$version .
 
+
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 898852898285.dkr.ecr.us-east-2.amazonaws.com
+
+docker tag apprepo:$version 898852898285.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
 
 docker tag apprepo:$version 898852898285.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
 
