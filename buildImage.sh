e05132813a704c38f61965bf8de28b07ada95715
aslamfam <81439199+aslamfam@users.noreply.github.com>
2021-04-15 11:51:48 +0530

    Update buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -1,7 +1,7 @@
 #This script will create docker image
 $version=$BUILD_ID
-docker build -t apprepo .
+docker build -t apprepo:v7 .
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 898852898285.dkr.ecr.us-east-2.amazonaws.com
-docker tag apprepo:$version 898852898285.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
-docker push 898852898285.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
+docker tag apprepo:v7 898852898285.dkr.ecr.us-east-2.amazonaws.com/apprepo:v7
+docker push 898852898285.dkr.ecr.us-east-2.amazonaws.com/apprepo:v7
 echo "Image Successfully pushed to ecr"
