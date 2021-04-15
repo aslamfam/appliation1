a0312b4100fe2f263256e3eaf7366a4904492b82
aslamfam <81439199+aslamfam@users.noreply.github.com>
2021-04-15 12:11:03 +0530

    Delete buildImage.sh

--- buildImage.sh
+++
@@ -1,6 +0,0 @@
-#This script will create docker image
-docker build -t testrepo .
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 898852898285.dkr.ecr.us-east-2.amazonaws.com
-docker tag testrepo:latest 898852898285.dkr.ecr.us-east-2.amazonaws.com/testrepo:latest
-docker push 898852898285.dkr.ecr.us-east-2.amazonaws.com/testrepo:latest
-echo "Image Successfully pushed to ecr"
